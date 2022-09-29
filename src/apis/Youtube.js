5faad848e510fa050e31dd8fe47479fbfafd243c
mosesglitch <mosesgachuru15@gmail.com>
2022-09-29 21:08:08 +0300

    add youtube api

---
+++ src/apis/Youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+const KEY = "AIzaSyBKs7Pp-_Zyfe1PqEqK5DKM_dZrCutHLmA";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY,
+  },
+});
