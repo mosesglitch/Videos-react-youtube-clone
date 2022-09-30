810eeee7fab51d0527f89b499d669a9405d66bbd
mosesglitch <mosesgachuru15@gmail.com>
2022-09-30 22:04:59 +0300

    add videoitem

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 const VideoItem = ({ video }) => {
-  return <div>{video.snippet.title}</div>;
+  return (
+    <div className="item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
 };
 export default VideoItem;
