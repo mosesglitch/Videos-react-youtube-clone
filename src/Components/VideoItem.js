584f79c5ef10ffb9b796c722f13e9c7577530022
mosesglitch <mosesgachuru15@gmail.com>
2022-09-30 23:10:31 +0300

    add grid to app.js

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./VideoItem.css";
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div onClick={() => onVideoSelect(video)} className="video-item item">
+    <div
+      alt={video.snippet.title}
+      onClick={() => onVideoSelect(video)}
+      className="video-item item"
+    >
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
